216247d55c0ea9723d84cb0c0024c7b6b99de1c4
Ilham Riana <ilhamriana3@gmail.com>
2021-12-26 21:34:56 +0700

    fixing mainscreen

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_banten/detail_screen.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -7,29 +8,42 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Katalog Wisata'),
       ),
-      body: Card(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Image.asset('images/farm-house.jpg'),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    'Farm House Lembang',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text('Lembang')
-                ],
+      body: InkWell(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return DetailScreen();
+          }));
+        },
+        child: Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Image.asset('images/farm-house.jpg'),
               ),
-            ),
-          ],
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        'Farm House Lembang',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('Lembang')
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
